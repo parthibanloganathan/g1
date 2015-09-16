4824408324d8b90de3a8b00ec268623c89a33b0e
Sagar Sarda <sagarsarda@gmail.com>
2015-09-15 20:26:42 -0400

    major restructuring changed a lot of things

--- Player.java
+++ Player.java
@@ -15,6 +15,7 @@ public class Player implements pppp.sim.Player {
     private Point[] random_pos = null;
     private Random gen = new Random();
     private Cell[][] grid = null;
+    private Point gate = null;
 
     // create move towards specified destination
     private static Move move(Point src, Point dst, boolean play) {
@@ -59,7 +60,7 @@ public class Player implements pppp.sim.Player {
             boolean neg_y = id == 2 || id == 3;
             boolean swap = id == 1 || id == 3;
             // first and third position is at the door
-            pos[p][0] = pos[p][2] = point(door, side * 0.5, neg_y, swap);
+            gate = pos[p][0] = pos[p][2] = point(door, side * 0.5, neg_y, swap);
             // second position is chosen randomly in the rat moving area
             pos[p][1] = null;
             // fourth and fifth positions are outside the rat moving area
@@ -69,7 +70,7 @@ public class Player implements pppp.sim.Player {
             pos_index[p] = 0;
         }
 
-        this.grid = createGrid(side, 10);
+        this.grid = createGrid(side, side/20);
     }
 
     private Cell[][] createGrid(int side, int slices) {
@@ -90,14 +91,16 @@ public class Player implements pppp.sim.Player {
             Point[][] pipers, boolean[][] pipers_played, Point[] rats
     ) {
         // for now ignore influence of other players
+    	for(Cell[] row : grid)
+    		for(Cell cell : row)
+    			cell.weight = 0;
         for (Point rat : rats) {
             Cell cell = getCell(rat);
             if (cell != null) cell.weight++;
         }
     }
 
-    void determinePiperDests(
-            Point[][] pipers, boolean[][] pipers_played, Point[] rats) {
+    void determinePiperDests(Point[][] pipers, boolean[][] pipers_played, Point[] rats) {
         // We're ignoring other inputs for now, just considering the
         // rats and the instance variable 'grid'
         ArrayList<Cell> cells = new ArrayList<Cell>();
@@ -106,47 +109,107 @@ public class Player implements pppp.sim.Player {
                 cells.add(cell);
         cells.sort(null);
         int n_rats = 0;
+        Iterator<Cell> cellIter = cells.iterator();
+        double avg_weight = rats.length/cells.size();
+        while(cellIter.hasNext()) {
+        	Cell cell = cellIter.next();
+        	if(cell.weight <= 2*avg_weight || distance(cell.center, gate) < 20) {
+        		cellIter.remove();
+        		continue;
+        	}
+        	n_rats += cell.weight;
+        }
         for (Cell cell : cells)
             n_rats += cell.weight;
         int n_pipers = pipers[id].length;
         ArrayList<Integer> unassigned_pipers = new ArrayList<Integer>();
-        for (int i = 0; i < n_pipers; ++i) unassigned_pipers.add(i);
+        for (int i = 0; i < n_pipers; ++i) if(pos_index[i] == 1 || pos_index[i] == 2) unassigned_pipers.add(i);
         for (int i = 0; i < cells.size(); ++i) {
-            // Probably need to reweight/increase this artificially too
-            int n_pipers_to_i = n_pipers * cells.get(i).weight / n_rats;
-            if (n_pipers_to_i == 0 || unassigned_pipers.size() == 0 ||
+            if (n_rats == 0 || unassigned_pipers.size() == 0 ||
                     cells.get(i).weight <= 1)
                 break;
+            // Probably need to reweight/increase this artificially too
+            int n_pipers_to_i = n_pipers * cells.get(i).weight / n_rats;
+            if(n_pipers_to_i == 0) break;
 
-            // Calculate distances of unassigned pipers to the points
-            double[] distances = new double[unassigned_pipers.size()];
+            double[] distances = new double[pipers[id].length];
+//            double[] distances = new double[unassigned_pipers.size()];
             for (int j = 0; j < distances.length; ++j) {
-                distances[j] = distance(
-                        cells.get(i).center,
-                        pipers[id][unassigned_pipers.get(j)]
-                );
+                distances[j] = unassigned_pipers.contains((Integer) j) ?
+                		distance(cells.get(i).center, pipers[id][j])
+                		: Double.MAX_VALUE;
             }
             double nth_smallest = quickSelect(distances, n_pipers_to_i);
             // Send pipers towards cell i
             for (int j = 0; j < distances.length; ++j)
-                if (distances[j] <= nth_smallest) {
+                if (distances[j] <= nth_smallest && distances[j] != Double.MAX_VALUE) {
                     // I'm abstracting away many details by using the
                     // current structure the TA gave for go to door ->
                     // go to location (he used random) -> back to door
                     // -> thru door -> repeat Simply updating random_pos here.
-                    Integer piper = unassigned_pipers.get(j);
+                    Integer piper = j;
                     random_pos[piper] = cells.get(i).center;
                     unassigned_pipers.remove((Integer) piper);
+                    if(distances[piper] > 20 && n_rats_near(pipers[id][piper],rats) < 3) pos_index[piper] = 1;
+                    distances[piper] = Double.MAX_VALUE;
                 }
         }
         // Possible (likely) in the case of few rats/sparse map that we
         // will have ONLY unassigned pipers. I'm also expecting a small
         // number of unassigned pipers dense maps.
         if (unassigned_pipers.size() > 0) {
-            //TODO what to do with unassigned pipers, especially in
-            // sparse maps (like the example). I'm thinking find rats
-            // closest to base and send closest piper to it?
+        	int n_unassigned = unassigned_pipers.size();
+        	double[] rat_dist_gate = new double[rats.length];
+        	for(int i = 0; i < rat_dist_gate.length; ++i) {
+        		rat_dist_gate[i] = distance(rats[i], gate);
+        		// We need to ignore any rats that are being brought in at the moment
+        		if(rat_dist_gate[i] <= side/2) rat_dist_gate[i] = (side - rat_dist_gate[i])/2;
+        	}
+        	// Ensure that there are at least as many rats as pipers
+        	// if not first only assign 1 piper to each rat first
+        	// Then we assign the rest of the pipers to the closest rat
+        	double nth_closest_rat = quickSelect(rat_dist_gate, Math.min(n_unassigned,rat_dist_gate.length));
+        	for(int i = 0; i < rat_dist_gate.length; ++i)
+        		if(rat_dist_gate[i] <= nth_closest_rat) {
+        			Integer closest_piper = null;
+        			double dist_closest = Double.MAX_VALUE;
+        			for(Integer piper : unassigned_pipers) {
+        				if(distance(pipers[id][piper], rats[i]) <= dist_closest) {
+        					dist_closest = distance(pipers[id][piper], rats[i]);
+        					closest_piper = Integer.valueOf(piper);
+        				}
+        			}
+					unassigned_pipers.remove((Integer) closest_piper);
+					random_pos[closest_piper] = rats[i];
+					if(unassigned_pipers.size() == 0) return;
+        		}
+        	// In case we had more pipers than rats, send to closest rat
+        	if(unassigned_pipers.size() > 0) {
+        		if(rats.length == 0) return;
+        		Iterator<Integer> iter = unassigned_pipers.iterator();
+        		while(iter.hasNext()) {
+        			Integer piper = iter.next();
+        			Point closest_rat_pos = null;
+        			double closest_rat_dist = Double.MAX_VALUE;
+        			for(int i = 0; i < rats.length; ++i) {
+        				double dist = distance(pipers[id][piper], rats[i]);
+        				if(dist < closest_rat_dist) {
+        					closest_rat_dist = dist;
+        					closest_rat_pos = rats[i];
+        				}
+        			}
+        			random_pos[piper] = closest_rat_pos;
+        			iter.remove();
+        		}
+        	}
         }
+    }
+    
+    static int n_rats_near(Point piper, Point[] rats) {
+    	int n = 0;
+    	for(int i = 0; i < rats.length; ++i)
+    		n += distance(piper,rats[i]) <= 10 ? 1 : 0;
+    	return n;
     }
 
     static double distance(Point a, Point b) {
@@ -177,15 +240,15 @@ public class Player implements pppp.sim.Player {
         determinePiperDests(pipers, pipers_played, rats);
 
         for (int p = 0; p != pipers[id].length; ++p) {
+        	if(pos_index[p] == 2 && n_rats_near(pipers[id][p], rats) < 1) --pos_index[p];
             Point src = pipers[id][p];
             Point dst = pos[p][pos_index[p]];
             // if null then get random position
             if (dst == null) dst = random_pos[p];
             // if position is reached
-            if (Math.abs(src.x - dst.x) < 0.000001 &&
-                    Math.abs(src.y - dst.y) < 0.000001) {
-                // discard random position
-                if (dst == random_pos[p]) random_pos[p] = null;
+            if ((Math.abs(src.x - dst.x) < 0.000001 &&
+                    Math.abs(src.y - dst.y) < 0.000001) || 
+                    (distance(src,dst) < 2 && dst == random_pos[p])) {
                 // get next position
                 if (++pos_index[p] == pos[p].length) pos_index[p] = 0;
                 dst = pos[p][pos_index[p]];
@@ -203,8 +266,17 @@ public class Player implements pppp.sim.Player {
 
     // Quickselect from internet
     public static double quickSelect(double[] input_arr, int k) {
-        if (input_arr == null || input_arr.length <= k)
+        if (input_arr == null)
             throw new Error();
+        if(input_arr.length == k) {
+        	double max = Double.MIN_VALUE;
+        	for(int i = 0; i < input_arr.length; ++i)
+        		if(max < input_arr[i])
+        			max = input_arr[i];
+        	return max;
+        }
+        if(input_arr.length < k)
+        	throw new Error();
 
         // copy to new array
         double[] arr = new double[input_arr.length];
